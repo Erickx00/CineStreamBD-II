7c8960e242d6ccb65b28a7b99cecff1ea79de18c
Erickx00 <erickdeveloperx10@gmail.com>
2026-08-27 08:56:08 -0300

    Add SQL script to truncate tables and reset identities in the database

---
+++ src/supabase/scripts_criacoes/derrubar_tabelas.sql
@@ -0,0 +1,18 @@
+TRUNCATE TABLE
+    visualizacao,
+    episodio,
+    obra_profissional,
+    obra_genero,
+    profissional,
+    genero,
+    obra,
+    perfil,
+    dispositivo,
+    fatura,
+    historico_assinatura,
+    assinante,
+    plano,
+    classificacao_maturidade,
+    status_cobranca,
+    qualidade_resolucao
+RESTART IDENTITY CASCADE;
